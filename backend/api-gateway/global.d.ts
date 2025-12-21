e4c5c24e6318de1495bf4f13ce6cc30adc7500e9
dulaksha keshan <dulakshakesh@gmail.com>
2025-12-21 17:47:52 +0530

    docker compose|db design template and services placeholders | firebase config -- 2

--- backend/api-gateway/global.d.ts
+++ backend/api-gateway/global.d.ts
@@ -2,8 +2,10 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT?: string;
+      FIREBASE_PROJECT_ID?: string;
+      FIREBASE_CLIENT_EMAIL?: string;
+      FIREBASE_PRIVATE_KEY?: string;
     }
   }
 }
-
 export { };
