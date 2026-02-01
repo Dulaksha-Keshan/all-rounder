571b951b4769e372c6e0f0d3865ac9897c17751a
Abdul-Malik-0404 <abdulmaliksadath@gmail.com>
2026-02-02 00:43:33 +0530

    added controllers to content services

---
+++ backend/content-service/src/controllers/postController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+// To creaete a post
+export const createPost = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {};
+
+// To fetch a mix of posts for the home screen
+export const getFeed = async (req: Request, res: Response): Promise<void> => {};
