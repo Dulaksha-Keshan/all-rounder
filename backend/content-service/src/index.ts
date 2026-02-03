2ebfa3fbbf1e53c0a824619eccfc281715cf1465
Pathumi <pathumikavee@gmail.com>
2026-02-03 19:39:59 +0530

    Add index.ts file for content service

---
+++ backend/content-service/src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import eventRoutes from "./routes/eventRoute.js";
+import postRoutes from "./routes/postRoute.js";
+import resourceRoutes from "./routes/resourceRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/events", eventRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/resources", resourceRoutes);
+
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("MONGO_URI is not defined in environment variables");
+}
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  });
+
+const PORT = process.env.PORT || 5003;
+
+app.listen(PORT, () => {
+  console.log(`Content service running on port ${PORT}`);
+});
